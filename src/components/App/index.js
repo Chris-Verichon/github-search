6337f1342fce8e2fd03accfdd2f957b4f5aaacdd
Chris-Verichon <chris.verichon@gmail.com>
2022-08-02 09:15:34 +0200

    feature FAQ donne

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,17 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import { Routes, Route } from 'react-router-dom';
+import githubLogo from 'src/assets/picture/logo-github.png';
+import Menu from '../Menu';
+import './styles.scss';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      <header>
+        <img src={githubLogo} alt="github logo" />
+        <Menu />
+      </header>
     </div>
   );
 }
